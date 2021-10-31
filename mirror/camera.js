529bed4db19c7666540eba8cc33f38bd41d14e90
Abidin Durdu <71967052+betelgeuse-7@users.noreply.github.com>
2021-10-31 16:57:36 +0300

    add mirror

---
+++ mirror/camera.js
@@ -0,0 +1,29 @@
+function hasGetUserMedia() {
+    // about double negation:
+    // https://stackoverflow.com/a/10467486
+    return !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia)
+}
+
+const cam = document.getElementById("cam")
+const stopCamBtn = document.getElementById("stop-cam-btn")
+const camSizeRangeInput = document.getElementById("cam-size")
+
+if (!hasGetUserMedia()) alert("no getUserMedia")
+else record()
+
+cam.style.width = "100%"
+cam.style.height = "100%"
+
+async function record() {
+    navigator.mediaDevices.getUserMedia({ video: true }).then(s => {
+        window.CAMSTREAM = s
+        cam.srcObject = s
+    })
+}
+
+function stopCam() {
+    const s = window.CAMSTREAM
+    s.getTracks().forEach(t => t.stop())
+}
+
+stopCamBtn.addEventListener("click", stopCam)
